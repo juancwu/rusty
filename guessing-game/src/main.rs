3014fd3763a503ff6f71f205a9e5612c5407f651
juancwu <46619361+juancwu@users.noreply.github.com>
2023-10-04 12:52:01 -0400

    working on guessing game

---
+++ guessing-game/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    println!("There is a randomly selected number between 1 to 100");
+
+    let number = rand::thread_rng().gen_range(1..=100);
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    loop {
+        println!("Please enter your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+        let guess: u32 = guess.trim().parse().expect("Please enter a number.");
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}

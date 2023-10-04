8b8e8ab0f155ab9177bfd19f5e6dc875e77cd23c
jc <46619361+juancwu@users.noreply.github.com>
2023-10-04 12:53:18 -0400

    in-progress: learning basic syntax

---
+++ basic-syntax/src/main.rs
@@ -0,0 +1,12 @@
+fn basic_fn() {
+    println!("This is a basic function");
+}
+
+
+fn main() {
+    println!("This file contains my practices of basic syntax in rust");
+
+    basic_fn();
+
+    // Variables
+}

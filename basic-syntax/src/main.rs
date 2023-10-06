c063654af49bb11caa28cba045d45f468e930079
jc <46619361+juancwu@users.noreply.github.com>
2023-10-05 22:57:36 -0400

    rust data types!

--- basic-syntax/src/main.rs
+++ basic-syntax/src/main.rs
@@ -38,4 +38,64 @@ fn main() {
     }
 
     println!("The value of x after the inner scope is: {x}");
+
+    // Data Types
+    //
+    // Integer Types (starts with i is signed, starts with u is unsigned)
+    // size | signed | unsigned |
+    // 8-bit i8 u8
+    // 16-bit i16 u16
+    // 32-bit i32 u32
+    // 64-bit i64 u64
+    // 128-bit i128 u128
+    // arch isize usize
+
+    // building in debug overflowing variables will go into "panic" - exit with error
+    // building in release, overflow will wrap around
+    
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+    
+    println!("Default floating point is 64-bit long: {x}");
+    println!("This is a 32-bit floating-point: {y}");
+
+    // Boolean
+    let falsy = false;
+    let truthy = true;
+
+    println!("Normal boolean values, falsy: {falsy}, truthy: {truthy}");
+
+    // Character type
+    let c = 'z';
+    let z: char = 'a';
+    let emoji = '👍';
+
+    println!("Character type: {c}, {z}, {emoji}");
+
+    // Tuple
+    let tup = (100, 2.1, 1);
+    let (x, y, z) = tup; // deconstruction
+    println!("The value of x, y, and z are: {x}, {y}, {z}");
+
+
+    // Arrays
+    let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "May",
+        "Apr",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dev"
+    ];
+    
+    println!("Months in an array: {:?}", months);
+
+    // let typed_array: [i32; 4] = [1, 2, 3, 4];
+    // let initial_value_array: [3; 5];
 }

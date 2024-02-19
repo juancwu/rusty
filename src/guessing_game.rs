b339a54f9e0321b45886fa8cf1ed64d48ffe9835
jc <46619361+juancwu@users.noreply.github.com>
2024-02-18 19:32:07 -0500

    complete guessing game continuous guessing until - non digit input - ctrl + c to terminate program

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -8,20 +8,25 @@ pub fn play() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("Please enter your guess:");
+    loop {
+        let mut guess = String::new();
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        println!("Please enter your guess:");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: i32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("Your guess: {guess}");
+        println!("Your guess: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
